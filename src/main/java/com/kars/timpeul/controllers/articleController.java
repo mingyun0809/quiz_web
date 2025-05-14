2771f11cd22ae9df74e014ace8688daddbe85e54
김민균 <rlaalsrbs@dgsw.hs.kr>
2025-05-14 15:10:18 +0900

    test

--- src/main/java/com/kars/timpeul/controllers/articleController.java
+++ src/main/java/com/kars/timpeul/controllers/articleController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping(value = "/article")
 public class articleController {
 
+
     @RequestMapping(value = "/playList", method = RequestMethod.GET, produces = MediaType.TEXT_HTML_VALUE)
     public String getPlayList() {return "article/playList";}
 
