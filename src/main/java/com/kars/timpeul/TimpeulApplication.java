ea046c86de6b56fc6fed57dcc2def2f9eaa039f6
arisue2002 <arisue2002@gmail.com>
2025-05-14 15:06:19 +0900

    first commit

---
+++ src/main/java/com/kars/timpeul/TimpeulApplication.java
@@ -0,0 +1,13 @@
+package com.kars.timpeul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimpeulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimpeulApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kars/timpeul/TimpeulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kars.timpeul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimpeulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
